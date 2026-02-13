d19ce32e8d9e3268f0efad059e64f135ec865efe
Aldyn Ishmael <93589643+aldnnnz@users.noreply.github.com>
2026-02-13 17:55:55 +0000

    refactor: migrate to astro (#1)

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+export default {
+  darkMode: ["class"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
